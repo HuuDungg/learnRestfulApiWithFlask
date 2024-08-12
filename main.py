7b15ad7f32ac4ff27c6e6cfecb9e47ece02fc32a
HuuDungg <huudung2004kt@gmail.com>
2024-08-12 16:06:48 +0700

    get all data and convert to dictionary

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from flask import Flask
+from flask import Flask, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from model import db, Item, Store
+
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@localhost/train06'
 
@@ -13,6 +14,21 @@ def index():
         return "Connection successful"
     except Exception as e:
         return f"Connection failed: {e}"
+
+@app.route("/getAllItems", methods=['GET'])
+def getAllItems():
+    items = Item.query.all()
+    # Chuyển đổi đối tượng Item thành danh sách các dictionary
+    items_list = [
+        {
+        'id': item.id,
+         'name': item.name, 
+         'price': item.price, 
+         'store_id': item.store_id
+         }
+           for item in items
+    ]
+    return jsonify(items_list)
 
 if __name__ == "__main__":
     with app.app_context():
