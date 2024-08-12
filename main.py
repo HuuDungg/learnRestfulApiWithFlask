98bfe184b6daf96cc2f56a784f23c0d31506ff43
HuuDungg <huudung2004kt@gmail.com>
2024-08-12 17:03:09 +0700

    create new data

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
 from flask_sqlalchemy import SQLAlchemy
 from model import db, Item, Store
 from schemas import ItemSchema, StoreSchema
+from marshmallow import ValidationError
 app = Flask(__name__)
+
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@localhost/train06'
 
 db.init_app(app)
@@ -37,6 +39,29 @@ def getById(id):
     item_schemas = ItemSchema()
 
     return jsonify(item_schemas.dump(item))
+
+@app.post("/create")
+def add_item():
+    # Lấy dữ liệu từ yêu cầu HTTP
+    json_data = request.get_json()
+
+    # Tạo schema và deserialze dữ liệu
+    item_schema = ItemSchema()
+    try:
+        item_data = item_schema.load(json_data)
+    except ValidationError as err:
+        return jsonify(err.messages), 400
+
+    # Tạo đối tượng Item mới
+    new_item = Item(**item_data)
+
+    # Thêm đối tượng vào cơ sở dữ liệu
+    db.session.add(new_item)
+    db.session.commit()
+
+    # Serialize đối tượng mới và trả về
+    result = item_schema.dump(new_item)
+    return jsonify(result), 201
 
 
 if __name__ == "__main__":
