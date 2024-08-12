3e3746a0e309cd48067b4a675ed9e10b77f00e50
HuuDungg <huudung2004kt@gmail.com>
2024-08-12 15:40:57 +0700

    create table in database throught SQLAlchemy model

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
+from model import db, Item, Store
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:12345678@localhost/train06'
-db = SQLAlchemy(app)
+
+db.init_app(app)
 
 @app.route('/')
 def index():
@@ -14,4 +15,6 @@ def index():
         return f"Connection failed: {e}"
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
